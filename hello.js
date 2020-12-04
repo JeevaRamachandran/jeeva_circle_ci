d9f3e632d97d5367ec6f337561dcc5d64ae7573a
Ramachandran <jramachandran@NTS205734MC.local>
2020-12-04 13:23:11 +0530

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,43 @@
-console.log("came inside");
+const { BigQuery } = require("@google-cloud/bigquery");
+const { bq } = require("./config.json");
+const main = async () => {
+  const { datasetId, projectId } = bq;
+  const table = `${projectId}.${datasetId}.__TABLES__`;
+  const query = `SELECT
+    project_id,
+    dataset_id,
+    REGEXP_EXTRACT(table_id,r'(.*)_') AS tab,
+TIMESTAMP_MILLIS(last_modified_time) last_updated_time,
+  FROM
+    ${table}
+--   WHERE
+--     DATE(TIMESTAMP_MILLIS(last_modified_time)) > DATE(DATE_ADD(CURRENT_TIMESTAMP(), -10, 'DAY'))
+  ORDER BY
+    last_modified_time DESC,
+    table_id DESC`;
+  const data = await execQuery(query);
+  console.log(data);
+};
+
+const { projectId, clientEmail, BQ_KEY } = bq;
+
+const gcpParams = {
+  projectId,
+  credentials: {
+    private_key: process.env.BQ_KEY
+      ? process.env.BQ_KEY.replace(/\\n/g, "\n")
+      : null,
+    client_email: clientEmail,
+  },
+};
+
+const execQuery = async (query, filters) => {
+  const bigquery = new BigQuery(gcpParams);
+  const options = {
+    query,
+  };
+  const [rows] = await bigquery.query(options);
+  return rows;
+};
+
+main();
